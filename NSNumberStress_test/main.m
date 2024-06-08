a05ae22250978e41aff8d1f25515789b29126d79
Gregory Casamento <greg.casamento@gmail.com>
2024-06-08 04:52:26 -0400

    Add readne

--- NSNumberStress_test/main.m
+++ NSNumberStress_test/main.m
@@ -5,12 +5,37 @@
 //  Created by Gregory John Casamento on 6/8/24.
 //
 
+// File: main.m
 #import <Foundation/Foundation.h>
+
+void stressTestNSNumber() {
+    // Create an NSNumber object
+    NSNumber *number = [[NSNumber alloc] initWithInt:42];
+
+    // Get the start time
+    NSDate *startTime = [NSDate date];
+    NSTimeInterval testDuration = 2 * 24 * 60 * 60; // 2 days in seconds
+
+    // Perform retain and release in a loop
+    while ([[NSDate date] timeIntervalSinceDate:startTime] < testDuration) {
+        for (int i = 0; i < 1000; i++) {
+            [number retain];
+            [number release];
+        }
+    }
+
+    // Release the initial reference
+    [number release];
+}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Starting stress test for NSNumber...");
+
+        // Call the stress test function
+        stressTestNSNumber();
+
+        NSLog(@"Stress test completed.");
     }
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSNumberStress_test
