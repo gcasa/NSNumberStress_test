2e4f4437c9e4224220be2a6ccc30581c9b2e081f
Gregory Casamento <greg.casamento@gmail.com>
2024-06-09 00:26:29 -0400

    Update to avoid issue

--- NSNumberStress_test/main.m
+++ NSNumberStress_test/main.m
@@ -8,24 +8,19 @@
 // File: main.m
 #import <Foundation/Foundation.h>
 
-void stressTestNSNumber() {
+void stressTestNSNumber(void)
+{
     // Create an NSNumber object
     NSNumber *number = [[NSNumber alloc] initWithInt:42];
 
-    // Get the start time
-    NSDate *startTime = [NSDate date];
-    NSTimeInterval testDuration = 2 * 24 * 60 * 60; // 2 days in seconds
-
     // Perform retain and release in a loop
-    while ([[NSDate date] timeIntervalSinceDate:startTime] < testDuration) {
+    while (TRUE)
+    {
         for (int i = 0; i < 1000; i++) {
             [number retain];
             [number release];
         }
     }
-
-    // Release the initial reference
-    [number release];
 }
 
 int main(int argc, const char * argv[]) {
@@ -34,8 +29,6 @@ int main(int argc, const char * argv[]) {
 
         // Call the stress test function
         stressTestNSNumber();
-
-        NSLog(@"Stress test completed.");
     }
     return 0;
 }
